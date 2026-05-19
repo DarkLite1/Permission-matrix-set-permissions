9ad81052224ea449e888a71dbe37b49cdd0c3181
DarkLite1 <brechtgijbels@gmail.com>
2026-05-19 15:38:12 +0200

    simplify mail banner

--- Modules/PermissionMatrix/Private/Html.ps1
+++ Modules/PermissionMatrix/Private/Html.ps1
@@ -316,13 +316,6 @@ function Build-ErrorWarningTableHC {
 
     if ($errs -eq 0 -and $warns -eq 0) { return '' }
 
-    $bannerColor = if ($errs -gt 0) {
-        $Script:Theme.AccentError
-    }
-    else {
-        $Script:Theme.AccentWarning
-    }
-
     $pills = @()
     if ($errs -gt 0) {
         $errLabel = "$errs Error" + $(if ($errs -ne 1) { 's' })
@@ -336,7 +329,7 @@ function Build-ErrorWarningTableHC {
     return @"
 <table role="presentation" cellpadding="0" cellspacing="0" border="0" width="100%" style="border-collapse:collapse; margin:0 0 16px 0;">
     <tr>
-        <td style='padding:12px 16px; background-color:$($Script:Theme.BgWhite); border:1px solid $($Script:Theme.BorderLight); border-left:4px solid $bannerColor; border-radius:8px;'>
+        <td style='padding:4px 0;'>
             <table role="presentation" cellpadding="0" cellspacing="0" border="0" style="border-collapse:collapse;">
                 <tr>
                     <td style='padding:0 12px 0 0; font-size:13px; font-weight:600; color:$($Script:Theme.TextMain);'>Detected issues</td>
